c3e50cbcc3da4faaf5989575c99ece45df7bd5aa
weix007 <56470129+weix007@users.noreply.github.com>
2022-05-10 15:40:58 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "3d72dd33-6f07-4f2b-9ee6-5eceba36327a",
                         "alterId": 0
                     }
                 ],
